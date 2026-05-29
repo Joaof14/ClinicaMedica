159d665980a8d85d3b2249505546759e271562b3
Ana-Jackeline <annajacke2@gmail.com>
2026-05-29 16:32:28 -0300

    Preparo da estrutura inicial de Paciente

---
+++ Paciente.java
@@ -0,0 +1,18 @@
+public class Paciente{
+    private float peso;
+    private float altura;
+    private String simtomas;
+
+    public void verPaciente(){
+
+    }
+
+    public void atualizarPaciente(){
+
+    }
+
+    public void deletarPaciente(){
+
+    }
+    
+}
